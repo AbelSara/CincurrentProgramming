2dd5d76da21d7159697636330b9baf72aed7d17c
zhh <nenbaying@163.com>
2020-01-19 17:31:22 +0800

    daemon thread

--- src/threadProfile/Daemon.java
+++ src/threadProfile/Daemon.java
@@ -5,4 +5,21 @@ package threadProfile;
  * @Describe daemon
  */
 public class Daemon {
+    public static void main(String[] args) {
+        new Thread(new DaemonThread(), "daemon_thread").start();
+    }
+
+    static class DaemonThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println("exit daemon thread!");
+            }
+        }
+    }
 }
