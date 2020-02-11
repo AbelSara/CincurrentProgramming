06866f3ed46913684099f5ef8143b34db182d710
zhh <nenbaying@163.com>
2020-02-11 16:06:16 +0800

    use of AbstractQueueLongSynchronizer

---
+++ src/lock/Mutex.java
@@ -0,0 +1,73 @@
+package lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedLongSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @Author Honghan Zhu
+ */
+public class Mutex implements Lock {
+    private static class Sync extends AbstractQueuedLongSynchronizer {
+        protected boolean isHeldExclusively() {
+            return getState() == 1;
+        }
+
+        public boolean tryAcquire(int acquires) {
+            if (compareAndSetState(0, 1)) {
+                setExclusiveOwnerThread(Thread.currentThread());
+                return true;
+            }
+            return false;
+        }
+
+        public boolean tryRelease(int releases) {
+            if (getState() == 0)
+                throw new IllegalMonitorStateException();
+            setExclusiveOwnerThread(null);
+            setState(0);
+            return true;
+        }
+
+        Condition newCondition() {
+            return new ConditionObject();
+        }
+    }
+
+    private Sync sync = new Sync();
+
+    @Override
+    public void lock() {
+        sync.acquire(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+        sync.acquireInterruptibly(1);
+    }
+
+    @Override
+    public boolean tryLock() {
+        if (sync.tryAcquire(1))
+            return true;
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        if (sync.tryAcquireNanos(time, unit.toNanos(time)))
+            return true;
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+        sync.release(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return sync.newCondition();
+    }
+}
