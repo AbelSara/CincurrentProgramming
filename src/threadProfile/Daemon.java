da76a3b088b3e1cc1b272298a69b98b680d85175
zhh <nenbaying@163.com>
2020-02-15 23:36:43 +0800

    daemon thread if no threads except daemon thread exit, JVM will exist

--- src/threadProfile/Daemon.java
+++ src/threadProfile/Daemon.java
@@ -1,14 +1,18 @@
 package threadProfile;
 
+import java.util.concurrent.ThreadPoolExecutor;
+
 /**
  * @Author Honghan Zhu
  * @Describe daemon
  */
 public class Daemon {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread daemonThread = new Thread(new DaemonThread(), "daemon_thread");
         daemonThread.setDaemon(true);
         daemonThread.start();
+        Thread.sleep(50);
+        System.out.println("main thread exit");
     }
 
     static class DaemonThread implements Runnable {
@@ -16,7 +20,9 @@ public class Daemon {
         @Override
         public void run() {
             try {
+                System.out.println("enter");
                 Thread.sleep(100);
+                System.out.println("exit");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
